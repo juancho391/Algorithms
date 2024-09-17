d3958bb2b4998a27bc49862881e4e58e84e7b1cf
Juan Bedoya <juanchischis2010@hotmail.com>
2024-09-16 21:32:09 -0500

    ordering bubble sort

---
+++ bublesort.py
@@ -0,0 +1,20 @@
+def bubleSort(lista:list)->list:
+    for j in range(len(lista)):
+        for i in range(0,len(lista)-1):
+            if lista[i] < lista[i+1]:
+                continue
+            else:
+                aux = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = aux
+    return lista
+    
+
+    
+
+
+
+
+lista = [3,5,4,2,1,9,8,10,15,20]
+
+print(bubleSort(lista))
